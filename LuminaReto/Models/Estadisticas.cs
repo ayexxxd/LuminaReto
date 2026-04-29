a24093f958ef44d396f4e6549b3f2ae5e286a7f4
Lya Salgado <A01424783@tec.mx>
2026-04-28 21:30:43 -0600

    Integracion completa
    Aqui ya metí todo mi codigo, subí mi HomeController (junté el de betsi con el mio), subí todos mis models (son 4), en View subí mi Index y en root subí mi carpeta de fotos y subí mi site, en index hay una linea casi hasta arriba que le especifica cual site.css usar,  o sea el mio

---
+++ LuminaReto/Models/Estadisticas.cs
@@ -0,0 +1,12 @@
+/*Este es el mondel de las tarjetas de arriba, las estadísticas*/
+
+namespace LuminaReto.Models /*Le dice que esto que estamos haciendo pertenece al proyecto de Ciclo1*/
+{
+    public class Estadisticas /*Crea la clase de Estadísticas, que será el molde que va a usar cada tarjeta pues de las estadísticas*/
+    {
+        public string Titulo {get; set;} /*get lee el dato y set guarda o cambia el dato*/
+        public string Valor {get; set;}
+        public string Icono {get; set;}
+        
+    }
+}
